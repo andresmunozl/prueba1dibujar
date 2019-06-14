6f5e3826473ef73d23492a8477aec92b77caab24
Andres M <jefferson@unicauca.edu.co>
2019-06-13 23:00:58 -0500

    First commit 1.2

--- app/src/main/java/com/andresmunozl/prueba1dibujar/MainActivity.java
+++ app/src/main/java/com/andresmunozl/prueba1dibujar/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 ruta.moveTo(posX,posY);
 
             }
-            //cambio
             if(accion== "moviendo"){
                 ruta.lineTo(posX,posY);
             }
